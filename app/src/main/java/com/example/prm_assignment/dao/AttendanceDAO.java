b6f21619c33b32d53ec25c02fa0556421c2e1e68
Bao <thaibao.nt@reso.vn>
2022-11-07 19:58:15 +0700

    Udapte AttendanceDAO (1)

--- app/src/main/java/com/example/prm_assignment/dao/AttendanceDAO.java
+++ app/src/main/java/com/example/prm_assignment/dao/AttendanceDAO.java
@@ -8,6 +8,7 @@ import androidx.room.Update;
 
 import com.example.prm_assignment.entities.Attendance;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface AttendanceDAO {
@@ -20,6 +21,6 @@ public interface AttendanceDAO {
     @Update
     void updateAttendance(Attendance attendance);
 
-    @Query("UPDATE Attendance set Status = (:status) WHERE Class_ID == (:class_id) AND Student_ID == (:student_id)")
-    Attendance checkAttendance (int class_id, int student_id, boolean status);
+    @Query("UPDATE Attendance set Status = (:status) WHERE Class_ID == (:class_id) AND Student_ID == (:student_id) AND Date == Date(:date)")
+    Attendance checkAttendance (int class_id, int student_id, boolean status, Date date);
 }
