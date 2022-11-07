14ce515d8f750c1e8a1bcabba180eb1e203e8fb5
Bao <thaibao.nt@reso.vn>
2022-11-07 20:29:00 +0700

    Update AttendanceDAO(2)

--- app/src/main/java/com/example/prm_assignment/dao/AttendanceDAO.java
+++ app/src/main/java/com/example/prm_assignment/dao/AttendanceDAO.java
@@ -22,5 +22,8 @@ public interface AttendanceDAO {
     void updateAttendance(Attendance attendance);
 
     @Query("UPDATE Attendance set Status = (:status) WHERE Class_ID == (:class_id) AND Student_ID == (:student_id) AND Date == Date(:date)")
-    Attendance checkAttendance (int class_id, int student_id, boolean status, Date date);
+    Attendance updateAttendance (int class_id, int student_id, boolean status, Date date);
+
+    @Query("SELECT * FROM Attendance WHERE Class_ID == (:class_id) AND Date == (:date)")
+    Attendance checkAttendance (int class_id, Date date);
 }
